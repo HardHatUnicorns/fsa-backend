b4ccf0b7280b0f0e8b6ef84e625c19cb01bd40a9
SamuelDidoszak <samueldidoszak1@gmail.com>
2022-12-14 22:01:35 +0100

    FSA-24 Added Cinema endpoint implementation

--- src/main/java/pl/aogiri/hhu/fsa/backend/cinema/web/controller/CinemaControllerImpl.java
+++ src/main/java/pl/aogiri/hhu/fsa/backend/cinema/web/controller/CinemaControllerImpl.java
@@ -1,8 +1,20 @@
 package pl.aogiri.hhu.fsa.backend.cinema.web.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
+import pl.aogiri.hhu.fsa.backend.cinema.application.CinemaService;
+import pl.aogiri.hhu.fsa.backend.cinema.application.dto.CinemaDto;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 public class CinemaControllerImpl implements CinemaController {
 
+    private final CinemaService cinemaService;
+
+    @Override
+    public List<CinemaDto> getCinemas() {
+        return cinemaService.getAllCinemas();
+    }
 }
