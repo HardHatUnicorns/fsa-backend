2f4a605d0e48e04de1b880b7ba5134169e5deb2b
Kaneki #Senapi <43042539+KanekiSenapi@users.noreply.github.com>
2022-12-14 19:08:27 +0100

    Update src/test/java/pl/aogiri/hhu/fsa/backend/cinema/application/mapper/CinemaFacilityDtoFixture.java

--- src/test/java/pl/aogiri/hhu/fsa/backend/cinema/application/mapper/CinemaFacilityDtoFixture.java
+++ src/test/java/pl/aogiri/hhu/fsa/backend/cinema/application/mapper/CinemaFacilityDtoFixture.java
@@ -3,6 +3,7 @@ package pl.aogiri.hhu.fsa.backend.cinema.application.mapper;
 import pl.aogiri.hhu.fsa.backend.cinema.application.dto.CinemaFacilityDto;
 
 public class CinemaFacilityDtoFixture {
+
     public static CinemaFacilityDto cinemaCityBonarka() {
         final CinemaFacilityDto cinemaFacilityDto = new CinemaFacilityDto();
         cinemaFacilityDto.setAddress("Henryka Kamieńskiego 11, 30-644 Kraków");
