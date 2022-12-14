2b11c40457809c53b0f58784f7b85ae1868ee74c
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-12-14 19:10:42 +0100

    Code clean
    Inspired By Michał O. :-)

--- src/test/java/pl/aogiri/hhu/fsa/backend/cinema/application/mapper/CinemaFacilityDtoFixture.java
+++ src/test/java/pl/aogiri/hhu/fsa/backend/cinema/application/mapper/CinemaFacilityDtoFixture.java
@@ -6,8 +6,8 @@ public class CinemaFacilityDtoFixture {
 
     public static CinemaFacilityDto cinemaCityBonarka() {
         final CinemaFacilityDto cinemaFacilityDto = new CinemaFacilityDto();
-        cinemaFacilityDto.setAddress("Henryka Kamieńskiego 11, 30-644 Kraków");
         cinemaFacilityDto.setId(1L);
+        cinemaFacilityDto.setAddress("Henryka Kamieńskiego 11, 30-644 Kraków");
         cinemaFacilityDto.setName("Cinema City Bonarka");
 
         return cinemaFacilityDto;
@@ -15,8 +15,8 @@ public class CinemaFacilityDtoFixture {
 
     public static CinemaFacilityDto cinemaCityGaleriaKazimierz() {
         final CinemaFacilityDto dto = new CinemaFacilityDto();
-        dto.setAddress("Podgórska 34, 31-536 Kraków");
         dto.setId(2L);
+        dto.setAddress("Podgórska 34, 31-536 Kraków");
         dto.setName("Cinema City Galeria Kazimierz");
 
         return dto;
